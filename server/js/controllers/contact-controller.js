6386ccbcd349ab9dcdf6f7527397c62cb5b565e7
KuraoHikari <dewaindra705@gmail.com>
2023-02-25 21:14:28 +0800

    update

---
+++ server/js/controllers/contact-controller.js
@@ -0,0 +1,14 @@
+const { Contact } = require("../models");
+
+async function addContact(req, res, next) {
+  try {
+  } catch (err) {
+    next(err);
+  }
+}
+async function deleteContact(req, res, next) {
+  try {
+  } catch (err) {
+    next(err);
+  }
+}
---
+++ server/js/controllers/message-controller.js
@@ -0,0 +1,9 @@
+const { Message, Contact } = require("../models");
+
+async function sendMessage(req, res, next) {
+  try {
+    const {} = req.body;
+  } catch (err) {
+    next(err);
+  }
+}
